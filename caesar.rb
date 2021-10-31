fb4230596d6a88d2b7b00879b874300bfd7aedb9
hykare <78385964+hykare@users.noreply.github.com>
2021-10-31 20:48:05 +0100

    shift a string of lowercase letters

--- caesar.rb
+++ caesar.rb
@@ -1,7 +1,12 @@
-string = 'What a string!'
+string = 'whatastring'
 
-def caesar_cipher(string, _shift)
+def caesar_cipher(string, shift)
   ascii_array = string_to_ascii(string)
+  p ascii_array
+  shifted_ascii_array = shift_ascii(ascii_array, shift)
+  p shifted_ascii_array
+  letter_array = ascii_to_char(shifted_ascii_array)
+  letter_array.join
 end
 
 def string_to_ascii(string)
@@ -9,4 +14,16 @@ def string_to_ascii(string)
   string.each_byte { |c| ascii_array.push c }
   ascii_array
 end
+
+def shift_ascii(ascii_array, shift)
+  lower_letters = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
+                   117, 118, 119, 120, 121, 122]
+  ascii_array.map { |ascii| lower_letters[ascii - 97 + shift - lower_letters.length] }
+end
+
+def ascii_to_char(ascii_array)
+
+  ascii_array.map { |ascii| ascii.chr }
+end
+
 p caesar_cipher(string, 5)
