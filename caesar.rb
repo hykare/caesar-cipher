36795b5d557e5fb285369c86feaaed8184f67bf5
hykare <78385964+hykare@users.noreply.github.com>
2021-10-31 18:38:24 +0100

    convert string to array of ascii codes

--- caesar.rb
+++ caesar.rb
@@ -1,7 +1,12 @@
 string = 'What a string!'
 
 def caesar_cipher(string, _shift)
-  string.split("")
+  ascii_array = string_to_ascii(string)
 end
 
+def string_to_ascii(string)
+  ascii_array = []
+  string.each_byte { |c| ascii_array.push c }
+  ascii_array
+end
 p caesar_cipher(string, 5)
