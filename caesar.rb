b46b13aa16a55586ac82689fd8ec7a1c52eaccbd
hykare <78385964+hykare@users.noreply.github.com>
2021-11-05 18:15:56 +0100

    change ascii code wrapping to use math instead of arrays

--- caesar.rb
+++ caesar.rb
@@ -14,17 +14,16 @@ def string_to_ascii(string)
 end
 
 def shift_ascii(ascii_array, shift)
-  downcase_codes = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
-                    117, 118, 119, 120, 121, 122]
-  upcase_codes = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-                  90]
+  lowcase_a_code = 97
+  upcase_a_code = 65
+  characters = 26
 
   ascii_array.map do |ascii_code|
     case ascii_code
     when 97..122
-      downcase_codes[ascii_code - downcase_codes[0] + shift - downcase_codes.length]
+      (ascii_code - lowcase_a_code + shift) % characters + lowcase_a_code
     when 65..90
-      upcase_codes[ascii_code - upcase_codes[0] + shift - upcase_codes.length]
+      (ascii_code - upcase_a_code + shift) % characters + upcase_a_code
     else
       ascii_code
     end
