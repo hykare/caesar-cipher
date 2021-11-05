3df2e0f85e4744b7ca07a8c26b2c888c89e9f164
hykare <78385964+hykare@users.noreply.github.com>
2021-11-05 17:42:21 +0100

    shift ascii based on character ranges

--- caesar.rb
+++ caesar.rb
@@ -1,10 +1,8 @@
-string = 'whatastring'
+string = 'What a string!'
 
 def caesar_cipher(string, shift)
   ascii_array = string_to_ascii(string)
-  p ascii_array
   shifted_ascii_array = shift_ascii(ascii_array, shift)
-  p shifted_ascii_array
   letter_array = ascii_to_char(shifted_ascii_array)
   letter_array.join
 end
@@ -16,13 +14,24 @@ def string_to_ascii(string)
 end
 
 def shift_ascii(ascii_array, shift)
-  lower_letters = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
-                   117, 118, 119, 120, 121, 122]
-  ascii_array.map { |ascii| lower_letters[ascii - 97 + shift - lower_letters.length] }
+  downcase_codes = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
+                    117, 118, 119, 120, 121, 122]
+  upcase_codes = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+                  90]
+
+  ascii_array.map do |ascii_code|
+    case ascii_code
+    when 97..122
+      downcase_codes[ascii_code - downcase_codes[0] + shift - downcase_codes.length]
+    when 65..90
+      upcase_codes[ascii_code - upcase_codes[0] + shift - upcase_codes.length]
+    else
+      ascii_code
+    end
+  end
 end
 
 def ascii_to_char(ascii_array)
-
   ascii_array.map { |ascii| ascii.chr }
 end
 
